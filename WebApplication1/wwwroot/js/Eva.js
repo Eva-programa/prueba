0f45b9057f8e13d29cd8bc5832c8d86a8891ce6d
Alumno <Alumno@DESKTOP-57OGPF5>
2024-03-27 14:12:06 +0100

    Agregar archivos de proyecto.

---
+++ WebApplication1/wwwroot/js/Eva.js
@@ -0,0 +1,12 @@
+﻿let boton = document.getElementById("enter");
+boton.addEventListener("click", pulsarBoton);
+function pulsarBoton() {
+    let primero = document.getElementById("numIntroducido");
+    let segundo = document.getElementById("numIntroducido2");
+    let valorprimero = primero.value;
+    let valorsegundo = segundo.value;
+    let numero = parseIn(valorprimero);
+    let numero2 = parseIn(valorsegundo);
+
+    if (numero>numero2)
+}
---
+++ WebApplication1/wwwroot/js/adivina.js
@@ -0,0 +1,15 @@
+﻿let boton = document.getElementById("boton");
+boton.addEventListener("click", pulsarBoton);
+
+function pulsarBoton() {
+    let elementoInputText = document.getElementById("numIntroducido");
+    let texto = elementoInputText.value;
+    let numero = parseInt(texto);
+
+    let elementoDiv = document.getElementById("salida");
+    if (numero > 17)
+        elementoDiv.className = "verde";
+    else
+        elementoDiv.className = "rojo";
+}
+ 
